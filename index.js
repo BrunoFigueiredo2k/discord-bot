98ded519637101f1638cc372b6ac7992ab63c751
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-11-15 18:41:15 +0100

    cleanup and now with cat command :)

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Import commands
+const commands = require('./commands')
+
 const fetch = require("node-fetch");
 const config = require('./.git/config.json');
 
@@ -16,9 +19,11 @@ const memesUrl = new URL('https://api.imgflip.com/get_memes');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  getMemes(memesUrl);
 });
 
 let memeImg;
+let memesCat = [];
 
 function getMemes(url){
   fetch(url)
@@ -30,11 +35,13 @@ function getMemes(url){
         return;
       }
 
-      // Examine the text in the response
+      // Get data from api and save it
       response.json().then(function(data) {
+        // Meme for images
         const randomNum = Math.floor(Math.random() * data.data.memes.length) + 1;
         memeImg = data.data.memes[randomNum].url;
-        console.log(memeImg);
+
+        createCatMeme(data);
       });
     }
   )
@@ -43,15 +50,31 @@ function getMemes(url){
   });
 }
 
-const commandMeme = '!meme';
+function createCatMeme(data){
+  // Check all names and if name includes 'cat' then add to memesCat empty array
+  for (i = 0; i < data.data.memes.length; i++){
+    const memeName = data.data.memes[i].name.toString();
+
+    if (memeName.toLowerCase().includes('cat')){
+      memesCat.push(data.data.memes[i].url.toString());
+    }
+  }
+}
   
 client.on('message', msg => {
-  if (msg.content === commandMeme){
-    getMemes(memesUrl)
+  getMemes(memesUrl);
+  if (msg.content === commands.generalMeme){ // Random meme
     msg.channel.send(memeImg);
+  } else if (msg.content == commands.catMeme){ // Random cat meme
+    // Random num for the length of cats array
+    const randomCatInt = Math.floor(Math.random() * memesCat.length) + 1; 
+    // Send random cat image in array
+    msg.channel.send(memesCat[randomCatInt]);
   }
 })
 
+
+// Test
 client.on('message', msg => {
   if (msg.content === '!bruno'){
     msg.reply('Bruno is 20 years old and a frontend developer at The Student Broker and KB Creative');
